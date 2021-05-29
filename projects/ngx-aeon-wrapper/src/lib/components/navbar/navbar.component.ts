c45ffcc84218c5012a109d256af2b8b3ee4095d5
newaeonweb <fsilvasmonteiro@gmail.com>
2021-05-29 17:19:58 -0300

    fix: reoved empty lifecycle

--- projects/ngx-aeon-wrapper/src/lib/components/navbar/navbar.component.ts
+++ projects/ngx-aeon-wrapper/src/lib/components/navbar/navbar.component.ts
@@ -1,13 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cw-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   @Input() public appTitle: string | undefined;
   constructor() {}
-
-  ngOnInit(): void {}
 }
